6fd72bb316e702c0dcf66be3a2df6e496c6dec93
Alexandre Syenchuk <as@netica.fr>
2016-01-27 16:06:06 +0100

    Version changed to 0.9.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ShelfCMS',
-    version='0.9.8',
+    version='0.9.9',
     url='https://github.com/iriahi/shelf-cms',
     license='BSD',
     author='Ismael Riahi',
