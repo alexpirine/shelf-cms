1c94f49d4256bfc44d3d74628b2c90347a541837
Alexandre Syenchuk <as@netica.fr>
2016-02-19 17:35:27 +0100

    Improved shortcut fields later menu rendering

--- shelf/admin/form.py
+++ shelf/admin/form.py
@@ -3,6 +3,9 @@ from shelf.plugins.order import OrderingInlineFieldList
 
 class ShortcutValidator(object):
     field_flags = ('shortcut',)
+    
+    def __init__(self, *args, **kwargs):
+        pass
 
 class ModelConverter(form.AdminModelConverter):
     def convert(self, model, mapper, prop, field_args, hidden_pk):
@@ -16,7 +19,7 @@ class ModelConverter(form.AdminModelConverter):
         form_shortcuts = getattr(self.view, 'form_shortcuts', None)
         
         if form_shortcuts and prop.key in form_shortcuts:
-            kwargs['validators'].append(ShortcutValidator())
+            kwargs['validators'].append(ShortcutValidator)
         
         res = super(ModelConverter, self).convert(model, mapper, prop, kwargs, hidden_pk)
         
--- shelf/static/shelf/js/hashchange.js
+++
@@ -1,6 +0,0 @@
-var shiftWindow = function() {
-  scrollBy(0, -95);
-  console.log('scroll');
-};
-if (location.hash) shiftWindow();
-window.addEventListener("hashchange", shiftWindow);
