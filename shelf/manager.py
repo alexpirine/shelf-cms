180634508796d8ac49bbc286b79844c821182ef7
Alexandre Syenchuk <as@netica.fr>
2016-03-08 14:21:09 +0100

    Improved Flask-Admin shelf sub-manager description

--- shelf/manager.py
+++ shelf/manager.py
@@ -1,9 +1,15 @@
 # coding: utf-8
 
-from flask.ext.script import Command, Manager
+from flask.ext.script import Manager
 from flask.ext.security import script as security_scrit
 
-manager = Manager(usage = "Runs Shelf management commands")
+MANAGER_DESC = "Shelf management commands"
+
+manager = Manager(
+    usage = None,
+    help = MANAGER_DESC,
+    description = MANAGER_DESC,
+)
 manager.add_command("create_user", security_scrit.CreateUserCommand())
 manager.add_command("activate_user", security_scrit.ActivateUserCommand())
 manager.add_command("deactivate_user", security_scrit.DeactivateUserCommand())
