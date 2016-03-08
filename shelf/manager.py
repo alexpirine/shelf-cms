ad913b2a0f411a4f4e90a2e17ba80fdd1675c2c3
Alexandre Syenchuk <as@netica.fr>
2016-03-08 16:09:22 +0100

    Typo fix in manager.py

--- shelf/manager.py
+++ shelf/manager.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from flask.ext.script import Manager
-from flask.ext.security import script as security_scrit
+from flask.ext.security import script as security_script
 
 MANAGER_DESC = "Shelf management commands"
 
@@ -10,9 +10,9 @@ manager = Manager(
     help = MANAGER_DESC,
     description = MANAGER_DESC,
 )
-manager.add_command("create_user", security_scrit.CreateUserCommand())
-manager.add_command("activate_user", security_scrit.ActivateUserCommand())
-manager.add_command("deactivate_user", security_scrit.DeactivateUserCommand())
-manager.add_command("create_role", security_scrit.CreateRoleCommand())
-manager.add_command("add_role", security_scrit.AddRoleCommand())
-manager.add_command("remove_role", security_scrit.RemoveRoleCommand())
+manager.add_command("create_user", security_script.CreateUserCommand())
+manager.add_command("activate_user", security_script.ActivateUserCommand())
+manager.add_command("deactivate_user", security_script.DeactivateUserCommand())
+manager.add_command("create_role", security_script.CreateRoleCommand())
+manager.add_command("add_role", security_script.AddRoleCommand())
+manager.add_command("remove_role", security_script.RemoveRoleCommand())
