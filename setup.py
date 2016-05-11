c3b1060a09ca52c9651144d8243369cd688b59a5
requires.io <support@requires.io>
2016-05-11 13:48:16 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ DEV_TOOLS = [
 if __name__ == '__main__':
     setup(
         name='ShelfCMS',
-        version='0.12.20',
+        version='0.12.22',
         url='https://github.com/iriahi/shelf-cms',
         license='BSD',
         author='Ismael Riahi',
