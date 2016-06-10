77c88635bb0096d2a11fc9dcc84142d36a118536
Alexandre Syenchuk <as@netica.fr>
2016-06-10 16:53:26 +0200

    [ecommerce] Added correct __module__ attribute to dynamically loaded classes

--- shelf/plugins/ecommerce/models.py
+++ shelf/plugins/ecommerce/models.py
@@ -9,6 +9,6 @@ __all__ = AM.__all__
 for class_name in __all__:
     settings_class = current_app.config.get('shelf.ec.models.%s' % class_name)
     if not settings_class:
-        globals()[class_name] = type(class_name, (getattr(AM, class_name),), {})
+        globals()[class_name] = type(class_name, (getattr(AM, class_name),), {'__module__': __name__})
     else:
         globals()[class_name] = settings_class
